5353a2733ab89e6a2eebe10204b3284085097674
stasyanski <157709692+stasyanski@users.noreply.github.com>
2025-02-07 20:35:16 +0000

    MariaDB dump - for database schema
    Tracking maria db dump to be used as schema

---
+++ database.sql
@@ -0,0 +1,184 @@
+-- MariaDB dump 10.19  Distrib 10.5.19-MariaDB, for Linux (x86_64)
+--
+-- Host: mysql    Database: news
+-- ------------------------------------------------------
+-- Server version	11.6.2-MariaDB-ubu2404
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `news`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `news` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+
+USE `news`;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoryId` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(5000) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (4,'1','Man wins prize',' Mauris sed eros mollis elit suscipit blandit. Suspendisse tristique vestibulum tortor eu aliquet. Curabitur a lacus vestibulum, efficitur sapien nec, ultricies sapien. Nam consequat cursus fermentum. Nullam sed facilisis justo. Donec non placerat urna. Cras feugiat vel lacus nec pellentesque. Aenean sed arcu ex. Duis elit ligula, tincidunt quis porta sed, iaculis ac sapien. Integer congue accumsan dui sit amet mattis. Sed at ipsum ac est blandit hendrerit eget quis orci. Aenean at porta turpis. Aliquam sit amet interdum mi. Praesent nec nisl id tortor bibendum commodo non sed nunc. Cras eu metus placerat, laoreet lectus a, lobortis turpis.\r\n\r\n','2024-11-26 19:31:03',1,NULL),(6,'3','Northampton football team wins championship','Integer tempus commodo quam, vitae vulputate ipsum porttitor sed. Vivamus hendrerit sapien eu placerat lacinia. Aliquam malesuada scelerisque ultricies. Proin vitae pellentesque ligula. Aliquam feugiat, ante fermentum scelerisque iaculis, purus mi ornare tortor, condimentum accumsan dolor mauris sed dui. Ut sit amet orci tincidunt, scelerisque nibh nec, dictum justo. In tempor lectus sed vulputate lobortis. Fusce quam lacus, feugiat in placerat eu, consectetur eget felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur semper tortor et tellus tempor, non rhoncus est varius. Sed elementum tellus non arcu dictum convallis. In non posuere quam, at interdum ligula. Nullam vestibulum arcu et pulvinar ornare. Ut varius arcu sit amet turpis rutrum, vitae ultricies est porta. ','2024-11-26 20:51:29',1,NULL),(7,'2','Northampton Christmas Lights Turned On',' Mauris sed eros mollis elit suscipit blandit. Suspendisse tristique vestibulum tortor eu aliquet. Curabitur a lacus vestibulum, efficitur sapien nec, ultricies sapien. Nam consequat cursus fermentum. Nullam sed facilisis justo. Donec non placerat urna. Cras feugiat vel lacus nec pellentesque. Aenean sed arcu ex. Duis elit ligula, tincidunt quis porta sed, iaculis ac sapien. Integer congue accumsan dui sit amet mattis. Sed at ipsum ac est blandit hendrerit eget quis orci. Aenean at porta turpis. Aliquam sit amet interdum mi. Praesent nec nisl id tortor bibendum commodo non sed nunc. Cras eu metus placerat, laoreet lectus a, lobortis turpis. ','2024-11-26 20:51:51',1,NULL),(8,'1','First cat to ever become multi trillionaire','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2025-01-06 00:26:16',1,'/images/upload/cat_caviar.jpg'),(9,'2','Simon the Chipmunk becomes Prime Minister','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2025-01-06 00:28:30',1,'/images/upload/Simon-Alvin-and-the-chipmunks-.jpg'),(10,'3','Man Citys Pep Guaordiola is absolutely losing it all','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2025-01-06 00:30:33',1,'/images/upload/images.jpg'),(11,'7','Valve and Steam OS single handedly pushing gaming on Linux','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2025-01-06 00:36:00',1,'/images/upload/steam-deck-oled.jpg'),(12,'6','RED Weather Alert UK hit by severe snow fall','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2025-01-06 00:38:59',1,'/images/upload/images-snow.jpg');
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Local News'),(2,'Local Events'),(3,'Sport'),(4,'Business'),(6,'Climate'),(7,'Tech World');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `articleId` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT 'Guest',
+  `email` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `text` varchar(5000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,4,'John','Deere','Guest','johndeere@deere.co.uk','2025-01-06 00:21:12','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),(2,8,'Eso','Teric','esoteric','esoteric@gs.pub','2025-01-06 00:42:54','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),(3,8,'Eso','Teric','esoteric','esoteric@gs.pub','2025-01-06 00:42:58','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),(4,8,'Eso','Teric','esoteric','esoteric@gs.pub','2025-01-06 00:43:00','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),(5,10,'Eso','Teric','esoteric','esoteric@gs.pub','2025-01-06 00:43:07','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),(6,10,'Eso','Teric','esoteric','esoteric@gs.pub','2025-01-06 00:43:11','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),(7,11,'Linus','Torvalds','Guest','torvalds@linux.kernel','2025-01-06 00:45:04','HELL YEAH!'),(8,12,'Winter','Enjoyer','Guest','winterwins@winter.com','2025-01-06 00:45:44','HELL YEAH!'),(9,12,'Summer','Enjoyer','Guest','summerrocks@summer.com','2025-01-06 00:46:19','HORRIBLE NEWS~!');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inquiries`
+--
+
+DROP TABLE IF EXISTS `inquiries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inquiries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `inquiry` varchar(5000) NOT NULL,
+  `username` varchar(255) DEFAULT 'Guest',
+  `firstname` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `phone_num` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Pending',
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inquiries`
+--
+
+LOCK TABLES `inquiries` WRITE;
+/*!40000 ALTER TABLE `inquiries` DISABLE KEYS */;
+INSERT INTO `inquiries` VALUES (1,'I cant seem to be able to access the articles page','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','1','Arthur','Morgan','07488888888','arthurmorgan@rockstar.com','Pending','2025-01-06'),(2,'I dont like the banner on the main page','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','1','Arthur','Morgan','07488888888','arthurmorgan@rockstar.com','Complete','2025-01-06'),(3,'Under the right to be forgotten i request all data to be deleted','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','1','Lara','Croft','07488888888','ttr@lara.com','Pending','2025-01-06'),(4,'Test without being logged in','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','','John','Deere','07488888888','johndeere@deere.co.uk','Pending','2025-01-06');
+/*!40000 ALTER TABLE `inquiries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_num` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `permissions` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','$2y$10$.svVrBONsSTRqMIytd6rKu2sl9qT6yFcFiESyb7eBM3/2ZKI4WTry','admin@news.vje','07888888888','Dexter','Morgan',2),(2,'spidey','$2y$10$kK.W5U0zCoiZ53V2T.1/W.Nngq06UKnsbgTL1Uoc8qDBYQWgvTRbW','spiderman@gmail.com','07488888888','Peter','Parker',1),(3,'esoteric','$2y$10$HBGh8fYlSj6Qg.onNMwp7.1k69Siz6oY1XuUG73hh2sMjmIA19BzO','esoteric@gs.pub','07488888888','Eso','Teric',0),(4,'arthurmorgan','$2y$10$SlkQPpSiwM2MTatSigOmGOg5a.Ng0a4FACT5HWySR/j8hoUK.a9DG','arthurmorgan@rockstar.com','07488888888','Arthur','Morgan',0),(5,'thetombraider','$2y$10$e4TxMmmj6dLYnpgrNoW7xOyQDlsm0LHs.RKd9mnrBJ.gwPSIcM2oy','ttr@lara.com','07488888888','Lara','Croft',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'news'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-07 20:22:47
